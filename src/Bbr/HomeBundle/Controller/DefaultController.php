d05a12b98dd7cdfc9ecc33163cc746b4d0f55f96
BibiFock <bbr.bourin.avec.mention@gmail.com>
2013-03-14 23:56:40 +0100

    fix template issue + add footer and header template

--- src/Bbr/HomeBundle/Controller/DefaultController.php
+++ src/Bbr/HomeBundle/Controller/DefaultController.php
@@ -10,7 +10,7 @@ class DefaultController extends Controller
 {
 
     /**
- 	 * @Route("/{page}", defaults={"page" = "home"}, requirements={"page" = "^home|connect|register|price|contact|about$"})
+ 	 * @Route("/{page}", defaults={"page" = "home"}, requirements={"page" = "^home|connect|price|contact|about$"})
      * @Template()
      */
     public function indexAction($page)
@@ -20,10 +20,28 @@ class DefaultController extends Controller
 	
     /**
      * @Route("/register")
-     * @Template()
+	 * @Template()
      */
     public function registerAction()
     {
-        return array();
+        //$form = $this->get('form.factory')->create(new ContactType());
+
+        //$request = $this->get('request');
+        //if ('POST' == $request->getMethod()) {
+            //$form->bindRequest($request);
+            //if ($form->isValid()) {
+                //$mailer = $this->get('mailer');
+                //// .. setup a message and send it
+                //// http://symfony.com/doc/current/cookbook/email.html
+
+                //$this->get('session')->setFlash('notice', 'Message sent!');
+
+                //return new RedirectResponse($this->generateUrl('_demo'));
+            //}
+        //}
+
+        return array('form' => $form->createView());
+		//return $this->render('BbrHomeBundle:Default:register.html.twig', array( 'page' => 'bite'));
+		return array('page' => 'register');
     }
 }
